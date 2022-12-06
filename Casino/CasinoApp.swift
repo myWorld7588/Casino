fdb33ac10d2029fc55bdf2559eb7282d062d016d
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-05 20:36:10 -0500

    Initial Commit

---
+++ Casino/CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasinoApp.swift
+//  Casino
+//
+//  Created by Jake Choi on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
